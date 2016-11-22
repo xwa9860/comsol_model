9af4d7ba18a9768c13efb75f6750a33a96143bac
Yutong L <Yutong L>
2016-11-22 15:41:44 -0800

    calculate Tp2 Tp3 Tp4

--- build_model/create_ms_ht_in_pebble.m
+++ build_model/create_ms_ht_in_pebble.m
@@ -2,7 +2,7 @@
 model.physics.create('htpb', 'CoefficientFormPDE', 'geom1');
 model.physics('htpb').identifier('htpb');
 model.physics('htpb').field('dimensionless').field('Tp');
-model.physics('htpb').field('dimensionless').component({'Tp2' 'Tp3' 'Tp4' 'Tp5'});
+model.physics('htpb').field('dimensionless').component({'Tp2' 'Tp3' 'Tp4'});
 model.physics('htpb').selection.set([2]);
 
 model.physics('htpb').label('Heat diffusion in pebble');
@@ -14,8 +14,8 @@ init = model.physics('htpb').feature('init1');
 
 %% set matrix c
 k=0;
-for i=1:region-1
-    for j=1:region-1
+for i=1:region-2
+    for j=1:region-2
         cfeq.setIndex('c',{num2str(0), num2str(0)},k);
         k=k+1;
     end
@@ -23,65 +23,71 @@ end
 
 %% set matrix a
 k=0;
-for i=1:region-1
-    for j=1:region-1
-        if i<=region-2 && j<=region-2
+for i=1:region-2
+    for j=1:region-2
         if i==j
-            cfeq.setIndex('a', ['(rho_fuel*cp_fuel/k_fuel)*2*r',num2str(i),'/((r',num2str(i+2),'-r',num2str(i+1)...
+            cfeq.setIndex('a', ['k_fuel*2/((r',num2str(i+2),'-r',num2str(i+1)...
                 ')*(r',num2str(i+1),'-r',num2str(i),'))'],k);
         else
             cfeq.setIndex('a',num2str(0),k);
         end
-        else
-            if i==j
-           cfeq.setIndex('a',['(1','/(r',num2str(i+1),'-r',num2str(i),'))-','(h_conv/k_fuel)'],k);
-            else
-            cfeq.setIndex('a',num2str(0),k);  
-            end
-        end
+%         else
+%             if i==j
+%            cfeq.setIndex('a',['(1','/(r',num2str(i+1),'-r',num2str(i),'))+','(h_conv/k_fuel)'],k);
+%             else
+%             cfeq.setIndex('a',num2str(0),k);  
+%             end
+%         end
         k=k+1;
     end
 end
 
 %% set matrix da
 k=0;
-for i=1:region-1
-    for j=1:region-1
-        if i<=region-2 && j<=region-2
+for i=1:region-2
+    for j=1:region-2
+%         if i<=region-2 && j<=region-2
             if i==j
-            cfeq.setIndex('da', ['eigenMode*r',num2str(i+1)],k);
+            cfeq.setIndex('da','eigenMode*rho_fuel*cp_fuel',k);
             else
             cfeq.setIndex('da',num2str(0),k);
             end
-        else
-           cfeq.setIndex('da',num2str(0),k);
-        end
+%         else
+%            cfeq.setIndex('da',num2str(0),k);
+%         end
         k=k+1;
     end
 end
 %% set matrix f
 k=0;
-for i=1:region-1
+for i=1:region-2
     if i==1
-        cfeq.setIndex('f',['r',num2str(i+1),'*(1/3)*PdensityN/(rho_fuel)'...
-        '+(rho_fuel*cp_fuel/k_fuel)','*(((r',num2str(i+2),'*Tp',num2str(i+2),')/(r',num2str(i+2),...
+        cfeq.setIndex('f',['(1/3)*PdensityN/r',num2str(i+1),...
+        '+(1/r',num2str(i+1),')','*k_fuel*(((r',num2str(i+2),'*Tp',num2str(i+2),')/(r',num2str(i+2),...
         '-r',num2str(i+1),...
-        '))/(0.5*(r',num2str(i+2),'+r',num2str(i),')))'],k);
+        '))/(0.5*(r',num2str(i+2),'-r',num2str(i),')))'],k);
     else
-    if i<=region-2
-    cfeq.setIndex('f',['r',num2str(i+1),'*(1/3)*PdensityN/(rho_fuel)'...
-        '+(rho_fuel*cp_fuel/k_fuel)','*((r',num2str(i+2),'*Tp',num2str(i+2),')/(r',num2str(i+2),...
+    if i<=region-3
+    cfeq.setIndex('f',['(1/3)*PdensityN/r',num2str(i+1),...
+        '+(1/r',num2str(i+1),')','*k_fuel*((r',num2str(i+2),'*Tp',num2str(i+2),')/(r',num2str(i+2),...
         '-r',num2str(i+1),')+','(r',num2str(i),'*Tp',num2str(i),')/(r',num2str(i+1),'-r',num2str(i),...
-        '))/(0.5*(r',num2str(i+2),'+r',num2str(i),'))'],k);
+        '))/(0.5*(r',num2str(i+2),'-r',num2str(i),'))'],k);
     else
-        cfeq.setIndex('f',['-h_conv/k_fuel*T_flibe','+','Tp',num2str(i),'/(r',num2str(i+1),'-r',...
-            num2str(i),')'],k);
+        cfeq.setIndex('f',['(1/3)*PdensityN/r',num2str(i+1),...
+        '+(1/r',num2str(i+1),')','*k_fuel*((r',num2str(i+2),...
+        '*(((-h_conv/k_fuel)*T_flibe+Tp',num2str(i+1),'/(r',num2str(i+2),'-r',num2str(i+1),...
+        '))/(1/(r',num2str(i+2),'-r',num2str(i+1),')-h_conv/k_fuel))',...
+        ')/(r',num2str(i+2),...
+        '-r',num2str(i+1),')+','(r',num2str(i),'*Tp',num2str(i),')/(r',num2str(i+1),'-r',num2str(i),...
+        '))/(0.5*(r',num2str(i+2),'-r',num2str(i),'))'],k);
+%         cfeq.setIndex('f',['h_conv/k_fuel*T_flibe','+','Tp',num2str(i),'/(r',num2str(i+1),'-r',...
+%             num2str(i),')'],k);
     end
     end
     k=k+1;
 end
 % set intial condition for temperture
-for i=2:region
+for i=2:region-1
     init.set(['Tp', num2str(i)], 900);
 end
 
--- build_model/main.m
+++ build_model/main.m
@@ -14,7 +14,7 @@ if MultiScale
     run('create_ms_ht_in_pebble.m');
     model.variable.create('var25');
     model.variable('var25').model('mod1');
-    model.variable('var25').set('T_fuel', '(Tp2+Tp3+Tp4+Tp5)/4.0');
+    model.variable('var25').set('T_fuel', '(Tp2+Tp3+Tp4)/3.0');
 else
     run('create_ht_fuel.m');
 end
@@ -46,53 +46,53 @@ model.param.set('engenMode', '1', 'binary value for NON engenvalue mode(value = 
 run('create_steady_state_solver.m')
 model.sol('sol13').runAll;
 
-% %% Rerun eigenvalue calculation with temperature profile from steady state
-% % set to eigenvalue mode
-% model.param.set('engenMode', '0', 'binary value for NON engenvalue mode(value = 1 if not engenvalue mode, value =0 if engenvalue mode)');
-% % desable lambda
-% model.variable('var19').active(false);
-% % set the solver to take initial value from the previous steady state
-% % results
-% model.study('std2').feature('eigv').set('notsolnum', 'auto');
-% model.study('std2').feature('eigv').set('notsolmethod', 'sol');
-% model.study('std2').feature('eigv').set('usesol', 'on');
-% model.study('std2').feature('eigv').set('eigwhich', 'sr');
-% model.study('std2').feature('eigv').set('notstudy', 'std5');
-% model.study('std2').feature('eigv').set('shift', '1');
-% model.study('std2').feature('eigv').set('useinitsol', 'on');
-% model.sol('sol16').runAll;
-% 
-% % get new lambda
-% lambda_eigen_new = mphglobal(model, 'lambda');
-% fprintf('\nThe new eigenvalue is\n');
-% fprintf('%.10f ', lambda_eigen_new)
-% model.param.set('lambda_critical', lambda_eigen_new, 'lambda_engeinvalue to get to criticality');
+%% Rerun eigenvalue calculation with temperature profile from steady state
+% set to eigenvalue mode
+model.param.set('engenMode', '0', 'binary value for NON engenvalue mode(value = 1 if not engenvalue mode, value =0 if engenvalue mode)');
+% desable lambda
+model.variable('var19').active(false);
+% set the solver to take initial value from the previous steady state
+% results
+model.study('std2').feature('eigv').set('notsolnum', 'auto');
+model.study('std2').feature('eigv').set('notsolmethod', 'sol');
+model.study('std2').feature('eigv').set('usesol', 'on');
+model.study('std2').feature('eigv').set('eigwhich', 'sr');
+model.study('std2').feature('eigv').set('notstudy', 'std5');
+model.study('std2').feature('eigv').set('shift', '1');
+model.study('std2').feature('eigv').set('useinitsol', 'on');
+model.sol('sol16').runAll;
 
-% %% Steady state calculation
-% fprintf('\nRerun steady state calculation\n');
-% model.variable('var19').active(true);
-% model.param.set('engenMode', '1', 'binary value for NON engenvalue mode(value = 1 if not engenvalue mode, value =0 if engenvalue mode)');
-% model.sol('sol13').runAll;
-% 
-% %% Rerun eigenvalue calculation with temperature profile from steady state
-% % set to eigenvalue mode
-% model.param.set('engenMode', '0', 'binary value for NON engenvalue mode(value = 1 if not engenvalue mode, value =0 if engenvalue mode)');
-% % desable lambda
-% model.variable('var19').active(false);
-% model.sol('sol16').runAll;
-% % get new lambda
-% lambda_eigen_new = mphglobal(model, 'lambda');
-% fprintf('\nThe new eigenvalue is\n');
-% fprintf('%.10f ', lambda_eigen_new)
-% model.param.set('lambda_critical', lambda_eigen_new, 'lambda_engeinvalue to get to criticality');
-% 
-% %% Scale the flux to power
-% fprintf('\nScaling the flux and delayed neutron precursor concentration...\n');
-% run('create_scaling_study.m')
-% model.sol('sol15').runAll;
-% 
-% %% Transient calculation
-% fprintf('\nRunning transient...\n');
-% run('create_transient_study.m')
-% %run('create_transient_results')
-% %model.sol('sol4').runAll;
+% get new lambda
+lambda_eigen_new = mphglobal(model, 'lambda');
+fprintf('\nThe new eigenvalue is\n');
+fprintf('%.10f ', lambda_eigen_new)
+model.param.set('lambda_critical', lambda_eigen_new, 'lambda_engeinvalue to get to criticality');
+
+%% Steady state calculation
+fprintf('\nRerun steady state calculation\n');
+model.variable('var19').active(true);
+model.param.set('engenMode', '1', 'binary value for NON engenvalue mode(value = 1 if not engenvalue mode, value =0 if engenvalue mode)');
+model.sol('sol13').runAll;
+
+%% Rerun eigenvalue calculation with temperature profile from steady state
+% set to eigenvalue mode
+model.param.set('engenMode', '0', 'binary value for NON engenvalue mode(value = 1 if not engenvalue mode, value =0 if engenvalue mode)');
+% desable lambda
+model.variable('var19').active(false);
+model.sol('sol16').runAll;
+% get new lambda
+lambda_eigen_new = mphglobal(model, 'lambda');
+fprintf('\nThe new eigenvalue is\n');
+fprintf('%.10f ', lambda_eigen_new)
+model.param.set('lambda_critical', lambda_eigen_new, 'lambda_engeinvalue to get to criticality');
+
+%% Scale the flux to power
+fprintf('\nScaling the flux and delayed neutron precursor concentration...\n');
+run('create_scaling_study.m')
+model.sol('sol15').runAll;
+
+%% Transient calculation
+fprintf('\nRunning transient...\n');
+run('create_transient_study.m')
+%run('create_transient_results')
+%model.sol('sol4').runAll;
