23b6d9c7386650892f531b3b614acbbce7db7e78
Xin Wang <imwangxin@gmail.com>
2017-03-02 11:19:09 -0800

    clearup define_input_parameters file to give more meaningful msg

--- build_model/define_input_parameters.m
+++ build_model/define_input_parameters.m
@@ -5,22 +5,27 @@ gnb=8; % energy group number
 region=5; % temperature group number
 
 MultiScale= false;
-sp3 = true;
+sp3 = false;
 %Try to use ChiT for all the Chi's and use beta effective for delayed
 %neutrons
 BetaEff = true;
 
-OpPower = '1E7[W]' %string, input to comsol global variable 'Pop'
+OpPower = '1E7[W]'; %string, input to comsol global variable 'Pop'
 
 %% define transient study parameters
-tf = 20 %second, finishing time of the transient
-dt = 0.1 %second, time step to record the results(not the timestep that the solver takes)
+tf = 20; %second, finishing time of the transient
+dt = 0.1; %second, time step to record the results(not the timestep that the solver takes)
 
 %% define reactivity insertion value, 0 means that this simulation is not
 % about reactivity insertion
-rho_ext = 0% reactivity insertion value 
-
+rho_ext = 0;% reactivity insertion value 
+if rho_ext > 0
+    fprintf('Reactivity insertion %r', rho_ext);
+end
 
 %% define Overcooling transient parameters
-OCOnset = 100 %s, starting time of overcooling, a very large time means the overcooling is not simulated
-OCSlope = -10 %K/s, speed of decrease in inlet coolant temperature
+OCOnset = 100; %s, starting time of overcooling, a very large time means the overcooling is not simulated
+OCSlope = -10; %K/s, speed of decrease in inlet coolant temperature
+if OCOnset < tf
+    fprintf('Overcooling transient with slope %f', OCSlope);
+end
