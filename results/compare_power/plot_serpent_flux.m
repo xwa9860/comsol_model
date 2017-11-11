4144382e467b97f2468e38c0f2d68d74f1198843
Xin Wang <imwangxin@gmail.com>
2017-11-11 11:10:25 -0800

    changed power serpent input files

--- results/compare_power/plot_serpent_flux.m
+++ results/compare_power/plot_serpent_flux.m
@@ -4,21 +4,20 @@ function plot_serpent_flux()
     R = 0.00:0.02:1.78;
     Z = 0.41:0.02:5.73;
     
-    serpent_output = 'two_fuelsgr_rods/serp_full_core_det0.m'; %when the control
+    serpent_output = 'two_fuels_gr_rods_shield/serp_full_core_det0.m'; %when the control
     % rods channels are filled with graphite
-    % serpent_output =  'cr_all_up_for_flux_det0.m';
     %when the control
     % rods channels are filled with flibe
     power = read_det_output(serpent_output, rbin, zbin, 'DET1');
     plim = max( max(power));
-    plot_2d(R, Z, power, 'power', 'gr_power.png', plim);
+    plot_2d(R, Z, power, 'power', 'gr_power_shield.png', plim);
     
     thermal_flux = read_det_output(serpent_output, rbin, zbin, 'DET2');
     fast_flux = read_det_output(serpent_output, rbin, zbin, 'DET3');
     
     plim = max( max(thermal_flux));
-    plot_2d(R, Z, thermal_flux, 'thermal flux', 'gr_thermal_flux.png', plim);
+    plot_2d(R, Z, thermal_flux, 'thermal flux', 'gr_thermal_flux_shield.png', plim);
     
     plim = max( max(fast_flux));
-    plot_2d(R, Z, fast_flux, 'fast flux', 'gr_fast_flux.png', plim);
+    plot_2d(R, Z, fast_flux, 'fast flux', 'gr_fast_flux_shield.png', plim);
 
