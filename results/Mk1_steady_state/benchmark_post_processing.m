4f56cc0e01004a3b730b8f5e6c000eea2b860552
Xin Wang <imwangxin@gmail.com>
2017-11-27 13:01:10 -0800

    added power comparison results

--- results/Mk1_steady_state/benchmark_post_processing.m
+++ results/Mk1_steady_state/benchmark_post_processing.m
@@ -9,24 +9,24 @@ Z = 0.48:0.14:5.66;
 model.result.export('data1').set('gridx2', 'range(2.5[cm], 5[cm],175.5[cm])');
 model.result.export('data1').set('gridy2', 'range(48[cm],14[cm],566[cm])');
 %% no rod at all
-diffusion_output = '1st_eig_power_mesh_572';
-serpent_output = 'rods_572_coarse/serp_full_core_det0.m';
+diffusion_output = 'Mk1_rods_572/1st_eig_power_mesh.txt';
+serpent_output = 'serpent_ref_rods_572/serp_full_core_det0.m';
 read_and_plot(diffusion_output, serpent_output, R, Z, ' (no rods)')
 %keff_no_rod_comsol =;
 %keff_no_rod_serpent = 0.986167;
 
-% % rods at middle height
-% diffusion_output = 'power_all_middle.txt';
-% serpent_output = 'rods_272/serp_full_core_det0.m';
-% read_and_plot(diffusion_output, serpent_output, R, Z, 'rods272[cm]')
-% keff_mid_comsol = 1/1.0513637108 ;
-% 
-% % rods at down 
-% diffusion_output = 'power_all_down.txt';
-% serpent_output = 'rods_112/serp_full_core_det0.m';
-% read_and_plot(diffusion_output, serpent_output, R, Z, 'rods112[cm]')
-% keff_down_comsol = 1/1.1599989541 ;
-% keff_serpent = 0.86949;
+% rods at middle height
+diffusion_output = 'Mk1_rods_272/1st_eig_power_mesh.txt';
+serpent_output = 'serpent_ref_rods_272/serp_full_core_det0.m';
+read_and_plot(diffusion_output, serpent_output, R, Z, '(rods half inserted)')
+%keff_mid_comsol = 1/1.0513637108 ; old value, need to be updated
+
+% rods at down 
+diffusion_output = 'Mk1_rods_112/1st_eig_power_mesh.txt';
+serpent_output = 'serpent_ref_rods_112/serp_full_core_det0.m';
+read_and_plot(diffusion_output, serpent_output, R, Z, '(rods fully inserted)')
+%keff_down_comsol = 1/1.1599989541 ;
+%keff_serpent = 0.86949;
 % 
 % tmsr
 % rbin = 34;
