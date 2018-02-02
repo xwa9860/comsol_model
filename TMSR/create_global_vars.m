13765a4d88fd3ab0c7ddaf33dde115a1683a397d
Xin Wang <imwangxin@gmail.com>
2018-02-02 15:11:34 -0800

    changed material properties

--- TMSR/create_global_vars.m
+++ TMSR/create_global_vars.m
@@ -1,5 +1,5 @@
 
-model.param.set('v_inlet', '0.14*0.4[m/s]', 'upwards velocity, uniform in the core 0.13*0.4[m/s]');
+model.param.set('v_inlet', '0.14[m/s]', 'upwards velocity, uniform in the core 0.14[m/s]');
 model.param.set('T0_flibe', '1051[K]', 'initial temperature for flibe salt, 672[degC] for TMSR');
 model.param.set('T_inlet', '672[degC]','nominal value is 672');
 model.param.set('T0_fuel', '900[K]', 'initial temperature for fuel pebbles, 800[degC]');
