2e2a2fad94ee1083774e8ac0ca9385153297077e
Xin Wang <imwangxin@gmail.com>
2018-02-05 22:32:52 -0800

    corrected gr properties

--- build_model/therm/create_ht_fuel_resistance.m
+++ build_model/therm/create_ht_fuel_resistance.m
@@ -48,7 +48,8 @@ init = model.physics('ht_fuel').feature('init1');
 %% get thermal_properties
 [k_triso, cp_triso, rho_triso] = calc_triso_thermal_properties();
 [k_fuel, cp_fuel, rho_fuel] = calc_pb_thermal_properties();
-[k_gr, cp_gr, rho_gr] = [193, 684, 1960];
+[k_gr, cp_gr, rho_gr] = calc_gr_properties();
+
 k_g_core = 193;
 k_g_shell = 193;
 
---
+++ utilities/calc_gr_properties.m
@@ -0,0 +1,5 @@
+function [k_gr, cp_gr, rho_gr] = calc_gr_properties()
+
+k_gr = 193;
+cp_gr = 684;
+rho_gr = 1960;
