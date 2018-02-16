bca6f1599bb9058bbff769956e4d166caec99591
Xin Wang <imwangxin@gmail.com>
2018-02-15 22:31:14 -0800

    new simulation res

--- results/Mk1/Temp_fb/temp_fb_mk1.m
+++ results/Mk1/Temp_fb/temp_fb_mk1.m
@@ -1,6 +1,6 @@
 %% Serpent results
-fuel_temp = [300, 900, 1500];
-keff = [0.989230 0.958558 0.912400];
+fuel_temp = [300, 600, 900, 1200, 1500];
+keff = [9.89334E-01 9.87615E-01 9.58675E-01, 9.57168E-01 9.12511E-01];
 %comsol feedback coef for fuel
 fuel_comsol = -0.0000672071
 
