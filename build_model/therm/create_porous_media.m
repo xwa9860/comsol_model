fac70e86205a1f54a4ae2be61a2cae3daef6a51e
Xin Wang <imwangxin@gmail.com>
2018-04-26 15:43:15 -0700

    changed inlet boundary conditions;

--- build_model/therm/create_porous_media.m
+++ build_model/therm/create_porous_media.m
@@ -1,5 +1,5 @@
 global pm_domains out_bound1 out_bound2 out_bound3
-global in_bound_lower in_bound_center_curved in_bound_center_straight in_bound_up_curved in_bound_up_top;
+global in_bound_lower in_bound_center_low in_bound_center_curved in_bound_center_straight in_bound_up_curved in_bound_up_top;
 global dimNb;
 
 % create Brinkman module
@@ -62,7 +62,7 @@ model.component('mod1').physics('br').feature('out3').set('p0', '-0.1*rhoL*g');
 model.physics('br').create('inl1', 'InletBoundary', dimNb-1);
 model.physics('br').feature('inl1').selection.set(in_bound_lower);
 model.physics('br').feature('inl1').set('BoundaryCondition', 'MassFlow');
-model.physics('br').feature('inl1').set('mfr', 'mL*bottomInletFraction*0.95');
+model.physics('br').feature('inl1').set('mfr', 'mL*bottomInletFraction');
 model.physics('br').feature('inl1').set('U0in', '0.5');
 model.physics('br').feature('inl1').set('IT', '0.05');
 model.physics('br').feature('inl1').set('LT', '0.01[m]');
@@ -70,14 +70,23 @@ model.physics('br').feature('inl1').set('k0', '0.005[m^2/s^2]');
 model.physics('br').feature('inl1').set('ep0', '0.005[m^2/s^3]');
 model.physics('br').feature('inl1').set('om0', '20[1/s]');
 
-% middle inlet 
+%----------- center inlet 
+model.component('mod1').physics('br').create('inl3', 'InletBoundary', 2);
+model.component('mod1').physics('br').feature('inl3').selection.set(in_bound_center_low);
+model.component('mod1').physics('br').feature('inl3').set('U0in', 0.02);
+
+model.component('mod1').physics('br').create('inl6', 'InletBoundary', 2);
+model.component('mod1').physics('br').feature('inl6').selection.set(in_bound_center_curved);
+model.component('mod1').physics('br').feature('inl6').set('U0in', 0.05);
+
 % define inlet velocity for boundary condition
 model.func.create('an9', 'Analytic');
 model.func('an9').label('Inlet Velocity');
 model.func('an9').set('args', {'z'});
-% model.func('an9').set('expr', '(3.3-0.45*(2-z)^2)*0.016'); %bottom heavy
-model.func('an9').set('expr', '(2.9-0.1*(2.8-z)^2)*0.015'); %even distribution
-model.func('an9').set('plotargs', {'z' '1' '5'});
+model.func('an9').set('expr', '(2.85-0.05*(1.75-z)^4)*0.016'); %bottom heavy
+%model.func('an9').set('expr', '(3.3-0.45*(2-z)^2)*0.016'); %bottom heavy
+%model.func('an9').set('expr', '(2.9-0.1*(2.8-z)^2)*0.015'); %even distribution
+model.func('an9').set('plotargs', {'z' '1' '4.5'});
 model.func('an9').set('funcname', 'vel_in');
 
 model.physics('br').create('inl2', 'InletBoundary', dimNb-1);
@@ -90,18 +99,15 @@ model.component('mod1').physics('br').feature('inl2').set('k0', '0.005[m^2/s^2]'
 model.component('mod1').physics('br').feature('inl2').set('ep0', '0.005[m^2/s^3]');
 model.component('mod1').physics('br').feature('inl2').set('om0', '20[1/s]');
  
-model.component('mod1').physics('br').create('inl3', 'InletBoundary', 2);
-model.component('mod1').physics('br').feature('inl3').selection.set(in_bound_center_curved);
-model.component('mod1').physics('br').feature('inl3').set('U0in', 0.04);
 
 % upper inlet 
 model.component('mod1').physics('br').create('inl4', 'InletBoundary', 2);
 model.component('mod1').physics('br').feature('inl4').selection.set(in_bound_up_curved);
-model.component('mod1').physics('br').feature('inl4').set('U0in', 0.01);
+model.component('mod1').physics('br').feature('inl4').set('U0in', 0.005);
 
 model.component('mod1').physics('br').create('inl5', 'InletBoundary', 2);
 model.component('mod1').physics('br').feature('inl5').selection.set(in_bound_up_top);
-model.component('mod1').physics('br').feature('inl5').set('U0in', 0.005);
+model.component('mod1').physics('br').feature('inl5').set('U0in', 0.001);
 
 
 %% wall 
